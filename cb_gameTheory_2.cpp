ff2ca40f76a06948acdae90a21663f591c98dcf7
Akshat Jha <akshat.k.jha@gmail.com>
2023-09-01 22:11:03 +0530

    first commit

---
+++ cb_gameTheory_2.cpp
@@ -0,0 +1,216 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define ones(x) __builtin_popcountll(x)
+#define int int
+#define pii pair<int,int>
+#define mii map<int,int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+using namespace std;
+const int mod = 1e9+7;
+const int N = 2e5+5;
+
+////****************************segtree//****************************//
+
+int lazy[4*N+1];
+int segTree[4*N+1];
+int a[N];
+
+void build(int v, int tl, int tr)
+{
+    if(tl==tr){
+        segTree[v] = a[tl];
+    }
+    else{
+        int tm = (tl+tr)/2;
+        build(2*v,tl,tm);
+        build(2*v+1,tm+1,tr);
+        // segTree[v] = segTree[2*v] + segTree[2*v+1];
+    }
+}
+
+void updateRangeLazy(int ss, int se, int l, int r, int inc, int node){  // O(logn)
+    //First step - Never go down if you have lazy value at node, first resolve it
+    if(lazy[node]!=0){
+        segTree[node] = lazy[node];
+        //if not a leaf node
+        if(ss!=se){ // pass the lazy value to children
+            lazy[2*node] = lazy[node];
+            lazy[2*node+1] = lazy[node];
+        }
+        lazy[node] = 0;
+    }
+
+    //Out of bounds
+    if(l>se || r<ss){
+        return;
+    }
+
+    //Complete Overlap
+    if(ss>=l && se<=r){
+        segTree[node] = inc;
+        //Pass the lazy value to children
+        if(ss!=se){
+            lazy[2*node] = inc;
+            lazy[2*node+1] = inc;
+        }
+        return;
+    }
+
+    //Partial Overlap
+    int mid = (ss+se)/2;
+    updateRangeLazy(ss, mid, l, r, inc, 2*node);
+    updateRangeLazy(mid+1, se, l, r, inc, 2*node+1);
+
+    segTree[node] = segTree[2*node]+ segTree[2*node+1];
+    return;
+}
+
+int queryLazy(int *tree, int ss, int se, int qs, int qe, int index){  // O(logn)
+    //First step - Never go down if you have lazy value at node, first resolve it
+    if(lazy[index]!=0){
+        tree[index] = lazy[index];
+        //if not a leaf node
+        if(ss!=se){ // pass the lazy value to children
+            lazy[2*index] = lazy[index];
+            lazy[2*index+1] = lazy[index];
+        }
+        lazy[index] = 0;
+    }
+
+    //Complete Overlap
+    if(ss>=qs && se<=qe){
+        return tree[index];
+    }
+
+    //No Overlap
+    if(qe<ss || qs>se){
+        return INT_MAX;
+    }
+
+    //Partial Overlap
+    int mid = (ss+se)/2;
+    int left = queryLazy(tree, ss, mid, qs, qe, 2*index);
+    int right = queryLazy(tree, mid+1, se, qs, qe, 2*index+1);
+
+    return min(left, right); 
+}
+
+void printUpdatedArray(int node, int tl, int tr) {
+    if(lazy[node]!=0){
+        segTree[node] = lazy[node];
+        //if not a leaf node
+        if(tl!=tr){ // pass the lazy value to children
+            lazy[2*node] = lazy[node];
+            lazy[2*node+1] = lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    if (tl == tr) {
+        cout << segTree[node] << "\n";
+    } else {
+        int tm = (tl + tr) / 2;
+        printUpdatedArray(node * 2, tl, tm);
+        printUpdatedArray(node * 2 + 1, tm + 1, tr);
+    }
+}
+
+//****************************dsu****************************//
+class Graph{
+    int n, *rank, *parent;
+public:
+    Graph(int n){
+        this->n = n;
+        rank = new int[n];
+        parent = new int[n];
+        for(int i=0;i<n;i++){
+            rank[i] = 1;
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    void union_set(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x != y){
+            if(rank[x] < rank[y]) swap(x, y);
+            parent[y] = x;
+            rank[x] += rank[y];
+        }
+    }
+    int connected_components(){
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(parent[i] == i) ans++;
+        }
+        return ans;
+    }
+    ~Graph(){
+        delete [] rank;
+        delete [] parent;
+    }
+};
+
+int table[1000005];
+
+unordered_set<int> calculatedivisors(int n){
+    unordered_set<int> tt;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            if(n/i!=i){}
+                tt.insert(n/i);
+            tt.insert(i);
+        }
+    }
+    tt.insert(1);
+    return tt;
+}
+
+int calculateMex(unordered_set<int> Set){
+    int Mex = 0;
+    while (Set.find(Mex) != Set.end())
+        Mex++;
+    return (Mex);
+}
+
+int calculateGrundy(int n){
+    if (n == 1)
+        return 0;
+    if(table[n]!=-1) return table[n];
+    unordered_set<int> Set, s1 = calculatedivisors(n); 
+    for(auto i:s1){
+        Set.insert(calculateGrundy(i));
+    }
+    return table[n] = calculateMex(Set);
+}
+
+//****************************main****************************//
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int test; cin>>test;
+    while(test--){
+        memset(table, -1, sizeof(table));
+        int n; cin>>n;
+        vi a(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=calculateGrundy(a[i]);
+        }
+        if(ans!=0)
+            cout<<"1"<<endl;
+        else 
+            cout<<"2"<<endl;
+    }
+    return 0;
+}
